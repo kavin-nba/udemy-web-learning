58c937846bb51d641661cc5ae50f893e04ce987c
kavin-nba <kavin.nba@gmail.com>
2021-07-04 11:02:46 +1000

    try

---
+++ survey-form-20210527/python-projects/python-project-no3.py
@@ -0,0 +1,7 @@
+friend_1 = input("friend 1:")
+friend_2 = input("friend 2:")
+friend_3 = input("friend 3:")
+
+my_friend_list = [friend_1 + " , " + friend_2 + " , " + friend_3 ]
+
+print(my_friend_list)
